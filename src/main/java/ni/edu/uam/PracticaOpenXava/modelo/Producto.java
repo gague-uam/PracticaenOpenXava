0f7984e6a77e6edf02114bffa02ba85f719ccbf5
gague-uam <ggpaiz@uamv.edu.ni>
2025-11-03 15:46:16 -0600

    La entidad de Producto es mejorada, se le agrega precio, fotos y observaciones

--- src/main/java/ni/edu/uam/PracticaOpenXava/modelo/Producto.java
+++ src/main/java/ni/edu/uam/PracticaOpenXava/modelo/Producto.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import org.openxava.annotations.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity @Getter @Setter
 public class Producto {
 
@@ -18,4 +20,14 @@ public class Producto {
             optional=true) // La referencia puede estar sin valor
     @DescriptionsList // Así la referencia se visualiza usando un combo
     Categoria categoria; // Una referencia Java convencional
+
+    @Money // La propiedad precio se usa para almacenar dinero
+    BigDecimal precio; // BigDecimal se suele usar para dinero
+
+    @Files // Una galería de fotos completa está disponible
+    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galería
+    String fotos;
+
+    @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
+    String observaciones;
 }
