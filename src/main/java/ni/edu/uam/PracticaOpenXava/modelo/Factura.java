3b26eda29d6c6fdad458e070d70ffc036328d47a
gague-uam <ggpaiz@uamv.edu.ni>
2025-11-03 16:12:10 -0600

    Se hacen mejoras en la clase Factura

--- src/main/java/ni/edu/uam/PracticaOpenXava/modelo/Factura.java
+++ src/main/java/ni/edu/uam/PracticaOpenXava/modelo/Factura.java
@@ -11,6 +11,13 @@ import org.openxava.calculators.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones"
+)
 public class Factura {
 
     @Id
